25ccf18e3cea5cefe7d7becc46fe0b1c56d6547d
Carlos Echeverri <ca.echeverrid@uniandes.edu.co>
2021-09-12 05:35:07 -0500

    Se corrige modelo pacientes

--- microservicio_pacientes/modelo/modelo.py
+++ microservicio_pacientes/modelo/modelo.py
@@ -5,11 +5,10 @@ db = SQLAlchemy()
 
 class Paciente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombres = db.Column(db.String(150))
-    apellidos = db.Column(db.String(150))
-    telefono = db.Column(db.String(20))
-    email = db.Column(db.String([100))
-
+    nombres = db.Column(db.String[150])
+    apellidos = db.Column(db.String[150])
+    telefono = db.Column(db.String[20])
+    email = db.Column(db.String[100])
 
 class PacienteSchema(SQLAlchemyAutoSchema):
     class Meta:
