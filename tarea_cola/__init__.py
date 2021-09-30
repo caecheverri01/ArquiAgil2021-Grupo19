c09bb83852a98e1ff328dec9c436e88939d41dc3
farojasp1 <88790787+farojasp1@users.noreply.github.com>
2021-09-29 22:04:04 -0500

    Delete tarea_cola directory

--- tarea_cola/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tarea_cola import *
--- tarea_cola/tarea_cola.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-import requests
-
-celery_app = Celery(__name__, broker='redis://localhost:6379')
-
-@celery_app.task()
-def registrar_evento(trama):
-    data_post = {"trama": trama}
-    resp = requests.post(url='http://127.0.0.1:5001/monitor', json = data_post)
