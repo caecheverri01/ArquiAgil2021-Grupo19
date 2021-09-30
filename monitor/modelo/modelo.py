c0139a1eb63856038a5fef41dc4010a6865c9b1c
farojasp1 <88790787+farojasp1@users.noreply.github.com>
2021-09-29 22:03:52 -0500

    Delete monitor directory

--- monitor/modelo/modelo.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class EjecucionServicio(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    servicio = db.Column(db.String[128])
-    respuesta = db.Column(db.String[10])
-    tiempo = db.Column(db.Integer)
