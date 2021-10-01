2a17389cef4c93577945fcb999e51a6cce01e67b
m.gaitans <m.gaitans@uniandes.edu.co>
2021-10-01 16:09:41 -0500

    Se crea Micro servicio Paciente

---
+++ microservicio_pacientes/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+def create_app(config_name):
+    app = Flask(__name__)  
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///microservicio_paciente.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    return app
---
+++ microservicio_pacientes/modelo/modelo.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+db = SQLAlchemy()
+
+class Paciente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombres = db.Column(db.String[150])
+    apellidos = db.Column(db.String[150])
+    telefono = db.Column(db.String[20])
+    email = db.Column(db.String[100])
+
+
+class PacienteSchema(SQLAlchemyAutoSchema):
+    class Meta:
+         model = Paciente
+         include_relationships = True
+         load_instance = True
