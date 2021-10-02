cb3173b9e0e0ab306457a23f36dd751d5e71d5c9
Carlos Echeverri <cecheverri@mihhgs.com>
2021-10-02 17:52:37 -0500

    Se adiciona manejo de token de seguridad

--- microservicio_facturacion/vista/vista.py
+++ microservicio_facturacion/vista/vista.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
 from ..modelo import db, Facturacion, FacturacionSchema 
 
 facturacion_schema = FacturacionSchema()
 
 class VistaFacturacion(Resource):
 
+    @jwt_required()
     def get(self, id_factura):
         facturacion = Facturacion.query.filter(Facturacion.id == id_factura).first()
         db.session.commit()
--- microservicio_historia/vista/vista.py
+++ microservicio_historia/vista/vista.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required
 from ..modelo import db, Historia, HistoriaSchema 
 
 historia_schema = HistoriaSchema()
 
 class VistaHistoria(Resource):
 
+    @jwt_required()
     def get(self, id_historia):
         historia = Historia.query.filter(Historia.id == id_historia).first()
         db.session.commit()
