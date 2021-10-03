17605804666f5097f4e3b4c1be7ec3608c75894c
Carlos Echeverri <cecheverri@mihhgs.com>
2021-10-02 20:08:59 -0500

    Se modifica respuesta de los servicios

--- microservicio_historia/vista/vista.py
+++ microservicio_historia/vista/vista.py
@@ -8,10 +8,4 @@ class VistaHistoria(Resource):
 
     @jwt_required()
     def get(self, id_historia):
-        historia = Historia.query.filter(Historia.id == id_historia).first()
-        db.session.commit()
-
-        if historia is None:
-            return {'mensaje':'La historia no existe'}, 400 
-        else:            
-            return historia_schema.dump(historia.query.get_or_404(id_historia))
+        return {'mensaje':'Respuesta servicio historia clinica OK para ID: ' + str(id_historia)}, 200 
